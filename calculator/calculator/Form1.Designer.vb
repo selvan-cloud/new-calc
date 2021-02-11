4058d97abf490b83a0a56272c8427f21419e6ffe
selvan-cloud <78867896+selvan-cloud@users.noreply.github.com>
2021-02-11 21:48:10 +0530

    Add files via upload

---
+++ calculator/calculator/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.textbox1 = New System.Windows.Forms.TextBox()
+        Me.textbox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'textbox1
+        '
+        Me.textbox1.AllowDrop = True
+        Me.textbox1.Location = New System.Drawing.Point(377, 33)
+        Me.textbox1.Name = "textbox1"
+        Me.textbox1.Size = New System.Drawing.Size(100, 23)
+        Me.textbox1.TabIndex = 0
+        '
+        'textbox2
+        '
+        Me.textbox2.Location = New System.Drawing.Point(377, 74)
+        Me.textbox2.Name = "textbox2"
+        Me.textbox2.Size = New System.Drawing.Size(100, 23)
+        Me.textbox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(95, 214)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(266, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 15)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Value 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(275, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 15)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Value 2"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(189, 214)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Sub"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(286, 214)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Multiple"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(377, 214)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Divide"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(604, 379)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.textbox2)
+        Me.Controls.Add(Me.textbox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents textbox1 As TextBox
+    Friend WithEvents textbox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ calculator/calculator/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Public sum As Integer
+
+    Private Sub textbox1_TextChanged(sender As Object, e As EventArgs) Handles textbox1.TextChanged
+        If textbox1.Text = "" Or
+               IsNumeric(textbox1.Text) = False Then
+            MessageBox.Show("Please enter a numeric value")
+        End If
+    End Sub
+
+    Private Sub textbox2_TextChanged(sender As Object, e As EventArgs) Handles textbox2.TextChanged
+        If textbox2.Text = "" Or
+               IsNumeric(textbox2.Text) = False Then
+            MessageBox.Show("Please enter the numeric value")
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        sum = Val(textbox1.Text) + Val(textbox2.Text)
+        MessageBox.Show("The sum is:" & sum)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        sum = Val(textbox1.Text) - Val(textbox2.Text)
+        MessageBox.Show("The sum is:" & sum)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        sum = Val(textbox1.Text) * Val(textbox2.Text)
+        MessageBox.Show("The sum is:" & sum)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        sum = Val(textbox1.Text) / Val(textbox2.Text)
+        MessageBox.Show("The sum is:" & sum)
+    End Sub
+End Class
